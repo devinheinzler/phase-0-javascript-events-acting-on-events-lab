3eff945e65f852250f9182e0aed010e898d4833d
devinheinzler <devinheinzler271@gmail.com>
2022-09-21 10:20:22 -0500

    commit tree 2ac5f1d905819a91b4c9a9e518bb0b8a045271c7

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
+    if (left < 360) {
+        dodger.style.left = `${left + 5}px`;
+    }
+  }
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
+    if (left < 360) {
+        dodger.style.left = `${left - 5}px`;
+    }
+  }
